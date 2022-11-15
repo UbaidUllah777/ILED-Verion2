024bd30424da295d7f3629e660e1d64b3d0ca514
UbaidUllah777 <khanxx777@gmail.com>
2022-11-15 17:50:11 +0500

    ubaid

---
+++ js/ourProducts.js
@@ -0,0 +1,37 @@
+const ourProducts = [
+  // 3D Hologram
+  {
+    id: "",
+    productName: "",
+    productHeadingLine: "",
+    productDetail: "",
+    productImages: [
+      {
+        image: "",
+      },
+      {
+        image: "",
+      },
+      {
+        image: "",
+      },
+      {
+        image: "",
+      },
+    ],
+    DisplayProperties: [
+      {
+        propertyName: [
+          {
+            name: "Pixel Pitch",
+            Values: [{ value: "50A" }, { value: "70A" }, { value: "43A" }],
+          },
+          {
+            name: "Resolution",
+            Values: [{ value: "50A" }, { value: "70A" }, { value: "43A" }],
+          },
+        ],
+      },
+    ],
+  },
+];
