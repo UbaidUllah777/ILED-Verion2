45b0b3cb72f91ae632825a600dc085296567defa
Ubaid <khanxx777@gmail.com>
2022-12-07 17:04:37 +0500

    Ubaid

--- js/common.js
+++ js/common.js
@@ -4,6 +4,7 @@ const ElidHeaderFacebook = document.getElementById("idHeaderFacebook");
 const ElidHeaderTwitter = document.getElementById("idHeaderTwitter");
 const ElidHeaderInstagram = document.getElementById("idHeaderInstagram");
 const ElidHeaderGetQuote = document.getElementById("idHeaderGetQuote");
+const ElidHeaderGetQuote2 = document.getElementById("idHeaderGetQuote2");
 const ElidHeaderLogo = document.getElementById("idHeaderLogo");
 const ElidHeaderLinkHome = document.getElementById("idHeaderLinkHome");
 const ElidHeaderLinkAboutUs = document.getElementById("idHeaderLinkAboutUs");
@@ -50,42 +51,8 @@ title="Instagram"
 ><i class="fab fa-instagram"></i
 ></a>`;
 
-ElidHeaderGetQuote.innerHTML = `
-<svg
-  class="custom-svg-btn-background"
-  version="1.1"
-  xmlns="http://www.w3.org/2000/svg"
-  xmlns:xlink="http://www.w3.org/1999/xlink"
-  x="0px"
-  y="0px"
-  viewBox="0 0 210 70"
-  preserveAspectRatio="none"
->
-  <polygon
-    fill="none"
-    stroke="#D4D4D4"
-    stroke-width="2"
-    stroke-miterlimit="10"
-    points="7,5 185,5 205,34 186,63 7,63 "
-  />
-</svg>
-GET A QUOTE
-<svg
-  class="custom-svg-btn-arrow"
-  version="1.1"
-  viewBox="0 0 15.698 8.706"
-  width="17"
-  xml:space="preserve"
-  xmlns="http://www.w3.org/2000/svg"
-  xmlns:xlink="http://www.w3.org/1999/xlink"
->
-  <polygon
-    stroke="#FFF"
-    stroke-width="0.4"
-    fill="#FFF"
-    points="11.354,0 10.646,0.706 13.786,3.853 0,3.853 0,4.853 13.786,4.853 10.646,8 11.354,8.706 15.698,4.353 "
-  />
-</svg>`;
+ElidHeaderGetQuote.innerHTML = `GET A QUOTE <i class="fas fa-arrow-right ms-2"></i>`;
+ElidHeaderGetQuote2.innerHTML = `GET A QUOTE <i class="fas fa-arrow-right ms-2"></i>`;
 
 ElidHeaderLogo.src = `img/demos/industry-factory/logo.png`;
 // ElidHeaderLogo.style.width = `123`;
